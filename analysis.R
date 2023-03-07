cf37504be82abb352eec75f29a2aabc5f1faf341
Andrew Crisp <andrew.crisp@gmail.com>
2023-03-07 07:12:45 -0700

    Initial commit

---
+++ analysis.R
@@ -0,0 +1,40 @@
+library(dplyr)
+library(ggplot2)
+
+data_dir <- file.path("data","raw")
+
+# Data from
+# https://catalog.data.gov/dataset/electric-vehicle-population-data
+#data_url <- "https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD"
+#data_file <- file.path(data_dir, "Electric_Vehicle_Population_Data.csv")
+
+# https://catalog.data.gov/dataset/crime-data-from-2020-to-present
+data_url <- "https://data.lacity.org/api/views/2nrs-mtv8/rows.csv?accessType=DOWNLOAD"
+data_file <- file.path(data_dir, "Crime-Data-from-2020-to-Present.csv")
+
+if (!file.exists(data_file)) {
+  download.file(data_url, data_file)
+}
+
+crime_data <- read.csv(data_file)
+selected <- crime_data %>% 
+  select(TIME.OCC, LAT, LON) %>% 
+  mutate(hour = floor(TIME.OCC/100)) %>% 
+  mutate(LAT = as.factor(round(LAT, 2))) %>% 
+  mutate(LON = as.factor(round(LON, 2))) %>% 
+  select(-TIME.OCC)
+
+agged <- selected %>% 
+  group_by(hour, LAT, LON) %>%
+  summarise(n = n())
+
+agged %>% ggplot(aes(x = LON, y = LAT)) +
+  geom_bin2d() +
+  facet_grid()
+
+model <- lm(n ~ hour, data = agged)
+model.predict <- cbind(agged, predict(model, interval = 'confidence'))
+
+model.predict %>% ggplot(aes(hour,n)) +
+  geom_line(aes(hour, fit)) +
+  geom_ribbon(aes(ymin=lwr,ymax=upr), alpha=0.3)
